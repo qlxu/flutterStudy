588aa34b7f919cd66bf036e30cbfe1cba7d6d2f1
xuqilong <xuqilong@7mtt.cn>
2019-02-13 18:27:49 +0800

    //使用AppBar和 Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -52,25 +52,43 @@ class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Material 是UI呈现的“一张纸”
-    return new Material(
-      // Column is 垂直方向的线性布局.
-      child: new Column(
-        children: <Widget>[
-          new MyAppBar(
-            title: new Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
-          new Expanded(
-            child: new Center(
-              child: new Text('Hello, world!'),
-            ),
-          ),
+//    return new Material(
+//      // Column is 垂直方向的线性布局.
+//      child: new Column(
+//        children: <Widget>[
+//          new MyAppBar(
+//            title: new Text(
+//              'Example title',
+//              style: Theme.of(context).primaryTextTheme.title,
+//            ),
+//          ),
+//          new Expanded(
+//            child: new Center(
+//              child: new Text('Hello, world!'),
+//            ),
+//          ),
+//        ],
+//      ),
+//    );
+    return new Scaffold(
+      appBar: new AppBar(
+
+        leading: new IconButton(icon: new Icon(Icons.menu), onPressed: null),
+        title: new Text('title'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.search),tooltip: 'search', onPressed: null)
         ],
       ),
+      body: new Center(
+        child: new Text('hello world'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+          tooltip: 'add',
+          child: new Icon(Icons.add),
+          onPressed: null),
     );
   }
+
 }
 
 void main() {
